07b40e24688e21d054b991b4c8b3e25a156a0480
Christian Schuermann <christian.schuermann.90@gmail.com>
2022-08-09 17:44:20 +0200

    done

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
@@ -22,4 +22,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeer => {
+   res.render('random-beer', randomBeer[0])
+  })
+  .catch(error =>  console.log(error));
+
+})
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
